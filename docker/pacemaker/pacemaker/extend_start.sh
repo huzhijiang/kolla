bb49b2b8e700da6aef6b99eb00e7d0c444902fdb
Zhijiang Hu <huzhijiang@gmail.com>
2017-12-04 15:32:01 +0800

    add note about /var/run permission
    Signed-off-by: Zhijiang Hu <huzhijiang@gmail.com>

--- docker/pacemaker/pacemaker/extend_start.sh
+++ docker/pacemaker/pacemaker/extend_start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "extend start"
+
+# Let libqb to use file system based UDS.
+# permission of /var/run should be set to 777 to 
+# make cib pengine ... create UDS successfully.
 mkdir -p /etc/libqb
 touch /etc/libqb/force-filesystem-sockets
 
