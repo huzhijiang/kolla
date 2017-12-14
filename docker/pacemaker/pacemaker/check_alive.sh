2b6d2c3147ccaebcae6483d7bbea301445bf7397
liguomin <lo.guomin3@zte.com.cn>
2017-12-13 22:02:41 -0500

    add dummy_service.sh into pacemaker image
    Signed-off-by: liguomin <lo.guomin3@zte.com.cn>

--- docker/pacemaker/pacemaker/check_alive.sh
+++ docker/pacemaker/pacemaker/check_alive.sh
@@ -4,3 +4,8 @@
 # Failures return 1
 
 echo "show info" | socat unix-connect:/var/lib/kolla/haproxy/haproxy.sock stdio > /dev/null
+if [ $? -eq 0 ] ; then
+    attrd_updater -n haproxy_health -v 1 -q
+else
+    attrd_updater -n haproxy_health -v 0 -q
+fi
